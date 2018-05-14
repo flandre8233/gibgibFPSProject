091171fb425668e7930826635e6ae63176a46057
flandre8233 <flandre8233@gmail.com>
2018-05-14 14:24:14 +0800

    no message

---
+++ Assets/keyboardDetector.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class keyboardDetector {
+    public KeyCode targetKey;
+    bool _isHold;
+
+    public bool isHold {
+        get {
+            return _isHold;
+        }
+    }
+
+    public void update() {
+    }
+}
---
+++ Assets/playermovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : SingletonMonoBehavior<playermovement> {
+    public Vector2 moveForce;
+    Rigidbody playerRB;
+
+    // Use this for initialization
+    void Start() {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    public void changeMoveForceX(int x) {
+        moveForce = new Vector2(x,moveForce.y);
+    }
+    public void changeMoveForceY(int y) {
+        moveForce = new Vector2(moveForce.x,y);
+    }
+
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        playerRB.velocity += transform.rotation*(new Vector3(moveHorizontal,0.0f, moveVertical) * 20*Time.deltaTime) ;
+        //playerRB.AddForce(new Vector3(moveHorizontal,0, moveVertical) * 100 * Time.deltaTime);
+    }
+}
